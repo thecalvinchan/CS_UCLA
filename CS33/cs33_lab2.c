682f0baf9fc16cb0a4523bacda5d723053f93ba1
Calvin <calvin.chan.h@gmail.com>
2013-10-14 03:45:09 -0700

    Commented code and removed temp files. Should probably check if the system is little/big endian, right now it is hardcoded for little endian

--- CS33/cs33_lab2.c
+++ CS33/cs33_lab2.c
@@ -12,15 +12,22 @@ void dumper( unsigned char *x, int n, int sgn ) {
 /* *x is any address in accessible memory */
 /* n is the number of 4 x 4 byte words to print */
 /* sgn is +/-1 to indicate whether to go ascending (+1) or descending (-1) */
+    //Rounds down by 16
     int *temp = (int *) (& x);
     *temp >>= 4;
     *temp <<= 4;
 
     int i,j,k;
+    //Iterates through the rows of 4 x 4-byte words
     for (i=0;i<n;i++) {
         printf("%p  ",x+(sgn*16*i));
+        //Iterates through the 4-byte words in each row
         for (j=0;j<4;j++) {
+            //Iterates through each byte in a 4-byte word and accounts for little endian
             for (k=3;k>=0;k--) {
+                //x+(sgn*16*i) accounts for cur row and pos/neg sgn
+                //4+j accounts for current word in the row
+                //k accounts for current byte in the word
                 printf("%.2x",*(x+(sgn*16*i)+(4*j)+k));
             }
             printf("  ");
