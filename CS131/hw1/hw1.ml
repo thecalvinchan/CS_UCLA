edb3948253b59e572357ff284225cfff371008b6
Calvin <calvin.chan.h@gmail.com>
2014-04-13 03:30:21 -0700

    Finished hw1, haven't tested yet

--- CS131/hw1/hw1.ml
+++ CS131/hw1/hw1.ml
@@ -31,3 +31,23 @@ let rec compute_rec_func = fun f p x->
 let rec computed_periodic_point = fun eq f p x->
     if eq (compute_rec_func f p x) x then x
     else computed_periodic_point eq f p (f x);;
+
+let rec filter_blind_alleys = fun g->
+
+    let check_term = fun sym r->
+        match sym with
+            | T->true
+            | N->if contains (sym,_) (filter_blind_alleys r) then true
+                else false;; in
+
+        let rec check_rhs_term = fun rhs q->
+            match rhs with
+                | []->true
+                | h::t-> if check_term h q then check_rhs_term t g
+                    else false;; in
+
+    match g with
+        | []->g
+        | (lhs,rhs)::t-> if check_rhs_term rhs q then (lhs,rhs)::(filter_blind_alleys t)
+            else (filter_blind_alleys t);;
+
